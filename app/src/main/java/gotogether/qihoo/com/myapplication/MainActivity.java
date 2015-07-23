8d904017510523ae204b067f987dd20274bd3af9
lyndon1115 <lindong.123@hotmail.com>
2015-07-23 15:48:16 +0800

    finish time picker

--- app/src/main/java/gotogether/qihoo/com/myapplication/MainActivity.java
+++ app/src/main/java/gotogether/qihoo/com/myapplication/MainActivity.java
@@ -94,8 +94,8 @@ public class MainActivity extends ActionBarActivity
             // Only show items in the action bar relevant to this screen
             // if the drawer is not showing. Otherwise, let the drawer
             // decide what to show in the action bar.
-            getMenuInflater().inflate(R.menu.main, menu);
-            restoreActionBar();
+//            getMenuInflater().inflate(R.menu.main, menu);
+//            restoreActionBar();
             return true;
         }
         return super.onCreateOptionsMenu(menu);
